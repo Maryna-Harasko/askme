8965d1634229ea99fb61650ebda6d7a4bdb72091
Maryna-Harasko <mngarasko@gmail.com>
2023-01-12 09:59:03 +0200

    validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,45 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    User.create(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Ви зареєструвалися!"
+    else
+      flash.now[:alert] = "Ви невірно заповнили форму реєстрації"
 
-    redirect_to root_path, notice: "Ви зареєструвалися!"
+      render :new
+    end
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: "Дані користувача оновлено!"
+    else
+      flash.now[:alert] = "При спробі зберігти зміни виникла помилка!"
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: "Користувача видалено"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
 
